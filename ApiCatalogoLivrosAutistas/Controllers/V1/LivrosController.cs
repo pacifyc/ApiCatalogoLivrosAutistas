9549114619b30bc45d640bbead22c80d265f620a
Silvio_Sena <silviosenna.jl@gmail>
2021-10-02 14:12:58 -0300

    Criação das Classes
    Criação das classe Livros View  Model
    e Livros Input Model

---
+++ ApiCatalogoLivrosAutistas/Controllers/V1/LivrosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoLivrosAutistas.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> Obter()
+        {
+            return Ok();
+        }
+
+
+        [HttpGet("{idLivro:guid}")]
+        public async Task<ActionResult<object>> ( Guid idLivro )
+        {
+	        return Ok();
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<object>> InserirLiro(object jogo)
+        {
+            return Ok();
+        }
+
+
+        [HttpPut("{idLivro:guid}")]
+        public async Task<ActionResult AtualizarLivro(Guid idLivro, object livro)
+        {
+	        return Ok();
+        }
+
+    }
+}
